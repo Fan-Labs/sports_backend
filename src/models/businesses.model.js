44647f00aa16f6197d4e9cf284894b5daba7e4cf
Grant van Helsdingen <gvanhels@gmail.com>
2019-02-17 21:52:24 +0200

    updated business model

--- src/models/businesses.model.js
+++ src/models/businesses.model.js
@@ -6,9 +6,37 @@ const DataTypes = Sequelize.DataTypes;
 module.exports = function (app) {
   const sequelizeClient = app.get('sequelizeClient');
   const businesses = sequelizeClient.define('businesses', {
-    text: {
+    name: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    phoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    latitude: {
+      type: DataTypes.FLOAT,
+      allowNull: false
+    },
+    longitude: {
+      type: DataTypes.FLOAT,
+      allowNull: false
+    },
+    streetAddress: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    logoUrl: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    googlePlace: {
+      type: DataTypes.JSON,
+      allowNull: true
     }
   }, {
     hooks: {
@@ -20,7 +48,7 @@ module.exports = function (app) {
 
   // eslint-disable-next-line no-unused-vars
   businesses.associate = function (models) {
-    businesses.belongsTo(models.users)
+    businesses.belongsTo(models.users, {foreignKey: 'merchantId'})
     // Define associations here
     // See http://docs.sequelizejs.com/en/latest/docs/associations/
   };
--- src/services/businesses/businesses.hooks.js
+++ src/services/businesses/businesses.hooks.js
@@ -3,7 +3,22 @@ const { authenticate } = require('@feathersjs/authentication').hooks;
 module.exports = {
   before: {
     all: [ authenticate('jwt') ],
-    find: [],
+    find: [
+      (context) => {
+        console.log('finding all businesses')
+        console.log(context.params.user.id)
+        
+        //merchants can only fetch a list of their own businesses in the merchant portal
+        if(context.params.user.isMerchant) {
+          context.params.query = {
+            ...context.params.query,
+            merchantId: context.params.user.id
+          }
+        }
+
+        return context
+      }
+    ],
     get: [],
     create: [],
     update: [],
